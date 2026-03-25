29debad45d3aa4bbbf97373124bea9e9fd65e86f
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-03-24 21:42:09 -0300

    Add files via upload

--- Aulas20261/Aula01/astronomia/src/App.js
+++ Aulas20261/Aula01/astronomia/src/App.js
@@ -1,1 +1,26 @@
+import logo from './Terra.png';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="titulo">Planeta Terra</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Terra é o quinto maior planeta do Sistema Solar.
+        </p>
+        <a
+          className="App-link"
+          href="https://pt.wikipedia.org/wiki/Terra"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Saiba Mais...
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
