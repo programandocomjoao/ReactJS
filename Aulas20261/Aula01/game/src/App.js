56e848a94a88e975ef37f1606213ac2599a4877b
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-03-24 21:42:51 -0300

    Add files via upload

--- Aulas20261/Aula01/game/src/App.js
+++ Aulas20261/Aula01/game/src/App.js
@@ -1,1 +1,32 @@
+import './App.css'
 
+function Titulo() {
+  return(
+    <h1>Games Clássicos</h1>
+  )
+}
+
+function Game(props) {
+  return(
+    <ul>
+      <li>Título: {props.titulo}</li>
+      <li>Ano de lançamento: {props.ano}</li>
+      <li>Console: {props.console}</li>
+    </ul>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Titulo />
+      <Game titulo="Futebol" ano="1977" console="Telejogo" />
+      <Game titulo="Come-Come!" ano="1982" console="Odyssey" />
+      <Game titulo="Seaquest" ano="1983" console="Atari 2600" />
+      <Game titulo="Super Mario Bros" ano="1985" console="Nintendo(NES)" />
+      <Game titulo="Sonic The Hedgehog" ano="1991" console="Master System" />
+    </>
+  )
+}
+
+export default App
